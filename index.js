4166f5c1a2a1b6186a9fdd0dc27ee29822d32dcc
Hansa Dewmina <hansadewminasenevirathna@gmail.com>
2025-06-27 00:13:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const express = require('express');
+const cors = require('cors');   // 1. Require cors
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
     code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
+
+// 2. Setup CORS - replace with your actual InfinityFree domain
+const allowedOrigin = 'https://dew-md-pair.free.nf'; // <-- change this
+
+app.use(cors({
+  origin: allowedOrigin,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use('/qr', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+app.use('/pair', async (req, res, next) => {
+  res.sendFile(__path + '/pair.html');
+});
+
+app.use('/', async (req, res, next) => {
+  res.sendFile(__path + '/main.html');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:` + PORT);
+});
+
+module.exports = app;
