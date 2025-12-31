75d1056a5ddf40ef117336266b120b8ce6ead93a
Hansa Dewmina <hansadewminasenevirathna@gmail.com>
2025-12-31 23:47:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 7860;
-let server = require('./qr'),
-    code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
+
+// middleware FIRST
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
- Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+// routes
+const server = require('./qr');
+const code = require('./pair');
+
+app.use('/server', server);
+app.use('/code', code);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
